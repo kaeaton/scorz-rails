b5fc42f514667f213f12144820fa046fc9e72254
k-eaton <katrina.eaton@gmail.com>
2017-03-16 13:58:32 -0700

    update records model

--- app/models/record.rb
+++ app/models/record.rb
@@ -15,7 +15,12 @@
 class Record < ApplicationRecord
 
   validates :popo_id, presence: true, uniqueness: true
+  validates :category, presence: true
   validates :description, presence: true
+  validates :full_description, presence: true
+  validates :day_of_week, presence: true
+  validates :district, presence: true
+  validates :datetime, presence: true
   validates :lat, presence: true
   validates :long, presence: true
   # validates :sale, presence: true  # blocks entries where boolean is false.
