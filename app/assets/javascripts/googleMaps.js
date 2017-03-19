f12870acd9152b7aa32d6f3183427db20fb3d96d
k-eaton <katrina.eaton@gmail.com>
2017-03-19 11:26:31 -0700

    google map now appears

--- app/assets/javascripts/googleMaps.js
+++ app/assets/javascripts/googleMaps.js
@@ -1,3 +1,31 @@
+
+// function loadScript() {
+//         var script = document.createElement('script');
+//         script.type = 'text/javascript';
+//         script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDaEYUur-BKTCXlAYxayJbx5Scpju9Ia3M&sensor=false&callback=initialize";
+//         document.body.appendChild(script);
+//     }
+
+
+function initMap() {
+	var uluru = {lat: -25.363, lng: 131.044};
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 4,
+		center: uluru
+	});
+	var marker = new google.maps.Marker({
+		position: uluru,
+		map: map
+	});
+}
+
+
+console.log("googleMaps.js has been loaded.")
+
+// $(document).ready(function(){
+// 	// loadscript();
+// 	initMap();
+// })
 //  class GoogleMap {
 // 	 function initMap() {
 // 	        var uluru = {lat: 37.75, lng: -122.445};
