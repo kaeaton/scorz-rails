3613a611024548b36ec6d832f324e79aa024daee
k-eaton <katrina.eaton@gmail.com>
2017-03-22 17:13:57 -0700

    begin choosing if dealer

--- app/assets/javascripts/records.js
+++ app/assets/javascripts/records.js
@@ -1,14 +1,25 @@
 $(document).ready(function(){
-	$("#drug").change(function(event) {
-		event.preventDefault();
 
 
-		var theChosenOne = $(this).serializeArray();
-		// var method = $(this).attr('method');
-		// var text = $(this).find('#text_text').val()
-		// var data = $(this).serializeArray();
-		debugger;
-	})
+
+		$("#drug").change(function(event) {
+			event.preventDefault();
+
+			var theChosenOne = $(this).serializeArray();
+
+		
+			$("#dealers").change(function(event){
+				event.preventDefault();
+
+				var theSource = $(this).serializeArray();
+
+				debugger;
+			})
+
+		})
+
+
+
 
 
 })
