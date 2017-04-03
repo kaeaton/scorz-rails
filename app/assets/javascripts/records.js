079623125804b85e32d5227faa7c2b43dda08056
k-eaton <katrina.eaton@gmail.com>
2017-04-02 22:50:05 -0700

    add scorz v1.1 D3 code. Needs updating.

--- app/assets/javascripts/records.js
+++ app/assets/javascripts/records.js
@@ -28,6 +28,12 @@ $(document).ready(function(){
 	    }
 	}
 
+
+	// google overlay
+
+
+
+
 	// Dropdown and checkbox functions
 
 	function marijuanaIsBadMkay(chosenOne){
@@ -74,13 +80,47 @@ $(document).ready(function(){
 			data: formData,
 			success: function(incoming){
 				console.log(incoming);
+
+				var overlay = new googlemaps.OverlayView();
+				// Add the container when the overlay is added to the map.
+		        overlay.onAdd = function(){
+		            var layer = d3.select(this.getPanes().overlayLayer)
+		                        .append("div")
+		                        .attr("class", "scores");
+		        // Draw each marker as a separate SVG element.
+		        // We could use a single SVG, but what size would it have?
+		        overlay.draw = function() {
+		            var projection = this.getProjection(),
+		                padding = 12;
+
+		        var marker = layer.selectAll("svg")
+                              .data(d3.entries(incoming))
+                              .each(transform) // update existing markers
+                              .enter().append("svg:svg")
+                              .each(transform)
+                              .attr("class", "marker")
+                              .each(transform)
+
+	            var firstObject = Object.keys(ajaxResults)[0];
+	            var drugType = ajaxResults[firstObject][2]
+	            console.log(drugType);
+
+	            marker.append("svg:circle")
+	                .attr("r", 4)
+	                .style("fill", function(d) { return styling(drugType) })
+                  	.style({'stroke': 'black', 'stroke-width': 0.2})
+                  	.attr("cx", padding)
+                 	.attr("cy", padding)
+
+				function transform(d) {
+            		d = new google.maps.LatLng(d.value[1], d.value[0]);
+            		d = projection.fromLatLngToDivPixel(d);
+            		return d3.select(this)
+                		.style("left", (d.x - padding) + "px")
+                		.style("top", (d.y - padding) + "px");
+		            }
+	          	}
 			}
 		})
-
 	})
-
-
-
-
-
 })
